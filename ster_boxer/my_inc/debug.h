47183cab210ae6b27b315d46ec9dda94cabc0a1e
dolewdam <damian.dolewski@tieto.com>
2017-04-20 13:17:45 +0200

    DEBUG: uart DMA transmit

--- ster_boxer/my_inc/debug.h
+++ ster_boxer/my_inc/debug.h
@@ -15,8 +15,8 @@ void DEBUG_Init(void);
 void DEBUG_SendString(char * xString);
 void DEBUG_SendByte(uint8_t xData);
 
-void _printc(uint8_t xTxByte);
-void _print(const uint8_t * xString);
+void _printChar(uint8_t xTxByte);
+void _printString(const uint8_t * xString);
 void _printLine(void);
 void _printInt(int32_t xValue);
 void _printHex(uint8_t xByte);
--- ster_boxer/my_src/debug.c
+++ ster_boxer/my_src/debug.c
@@ -8,15 +8,42 @@
 #include "debug.h"
 #include "fifo.h"
 
+#define DEBUG_TX			GPIO_Pin_6
+#define DEBUG_BUFF_SIZE		128
+
+
 volatile fifo_t debug_fifo;
-volatile char DebugBuffer[TX_BUFF_SIZE];
+volatile char DebugBuffer[DEBUG_BUFF_SIZE] = "DMA Transmit test (UART1)\r\n";
 
 void DEBUG_Init(void)
 {
-    fifo_init(&debug_fifo, (void *)DebugBuffer, TX_BUFF_SIZE);
+//    fifo_init(&debug_fifo, (void *)DebugBuffer, TX_BUFF_SIZE);
 
     GPIO_InitTypeDef GPIO_InitStructure;
     USART_InitTypeDef USART_InitStructure;
+    DMA_InitTypeDef DMA_InitStructure;
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1 , ENABLE);
+
+	DMA_DeInit(DMA1_Channel2);
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&USART1->TDR;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)&DebugBuffer;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+	DMA_InitStructure.DMA_BufferSize = DEBUG_BUFF_SIZE;
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Enable;
+	DMA_Init(DMA1_Channel2, &DMA_InitStructure);
+
+	DMA_Cmd(DMA1_Channel2, ENABLE);
+	DMA_ITConfig(DMA1_Channel2, DMA_IT_TC, ENABLE);
+
+	NVIC_SetPriority(DMA1_Channel2_3_IRQn, 1); //to samo co uart2
+	NVIC_EnableIRQ(DMA1_Channel2_3_IRQn);
 
     USART_InitStructure.USART_BaudRate = 230400;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
@@ -35,8 +62,7 @@ void DEBUG_Init(void)
     /* Connect PXx to USARTx_Tx */
     GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_0);
 
-    /* Configure USART Tx, Rx as alternate function push-pull */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6; //PB6
+    GPIO_InitStructure.GPIO_Pin = DEBUG_TX; //PB6
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -46,27 +72,37 @@ void DEBUG_Init(void)
     /* USART configuration */
     USART_Init(USART1, &USART_InitStructure);
 
-	NVIC_SetPriority(USART1_IRQn, 3);
-	NVIC_EnableIRQ(USART1_IRQn);
-
+//	NVIC_SetPriority(USART1_IRQn, 3);
+//	NVIC_EnableIRQ(USART1_IRQn);
+	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
     /* Enable USART */
     USART_Cmd(USART1, ENABLE);
 
 }
 
+void DMA1_Channel2_IRQHandler(void)
+{
+	/* Test on DMA Transfer Complete interrupt */
+	if (DMA_GetITStatus(DMA1_IT_TC2) != RESET)
+	{
+		/* Clear DMA Transfer Complete interrupt pending bit */
+		DMA_ClearITPendingBit(DMA1_IT_TC2);
+	}
+}
+
 void DEBUG_SendString(char * xString)
 {
 #ifdef DEBUG_TERMINAL_USART
-	fifo_write(&debug_fifo, xString, strlen(xString));
-	USART_ITConfig( USART1, USART_IT_TXE, ENABLE );
+//	fifo_write(&debug_fifo, xString, strlen(xString));
+//	USART_ITConfig( USART1, USART_IT_TXE, ENABLE );
 #endif
 }
 
 void DEBUG_SendByte(uint8_t xData)
 {
 #ifdef DEBUG_TERMINAL_USART
-	fifo_write(&debug_fifo, &xData, 1);
-	USART_ITConfig( USART1, USART_IT_TXE, ENABLE );
+//	fifo_write(&debug_fifo, &xData, 1);
+//	USART_ITConfig( USART1, USART_IT_TXE, ENABLE );
 #endif
 }
 
@@ -91,7 +127,7 @@ void USART1_IRQHandler(void)
 //-----------------------------------------------------------------------------
 // FUNKCJA WYSYLA BAJT PRZEZ UART6
 //-----------------------------------------------------------------------------
-void _printc(uint8_t xTxByte)
+void _printChar(uint8_t xTxByte)
 {
 	DEBUG_SendByte(xTxByte);
 }
@@ -99,7 +135,7 @@ void _printc(uint8_t xTxByte)
 //-----------------------------------------------------------------------------
 // Wypisanie stringu xString na konsole
 //-----------------------------------------------------------------------------
-void _print(const uint8_t * xString)
+void _printString(const uint8_t * xString)
 {
 	DEBUG_SendString(xString);
 }
@@ -108,7 +144,7 @@ void _print(const uint8_t * xString)
 //-----------------------------------------------------------------------------
 void _printLine(void)
 {
-	_print(UC"_________________________________________________________________\r\n");
+	_printString(UC"_________________________________________________________________\r\n");
 }
 //-----------------------------------------------------------------------------
 // Wypisanie wartosci dziesietnej ze znakiem xValue na konsole
@@ -165,9 +201,9 @@ void _printHex(uint8_t xByte)
 //-----------------------------------------------------------------------------
 void _printParam(uint8_t * xString, int32_t xValue)
 {
-	_print(UC"\r\n");
-	_print(xString);
-	_print(UC": ");
+	_printString(UC"\r\n");
+	_printString(xString);
+	_printString(UC": ");
 	_printInt(xValue);
 }
 
@@ -176,8 +212,8 @@ void _printParam(uint8_t * xString, int32_t xValue)
 //-----------------------------------------------------------------------------
 void _error(const uint8_t * xString)
 {
-	_print(UC"\r\ne: ");
-	_print(xString);
+	_printString(UC"\r\ne: ");
+	_printString(xString);
 }
 
 //-----------------------------------------------------------------------------
@@ -185,7 +221,7 @@ void _error(const uint8_t * xString)
 //-----------------------------------------------------------------------------
 void _errorc(const uint8_t * xString)
 {
-	_print(xString);
+	_printString(xString);
 }
 
 //-----------------------------------------------------------------------------
@@ -193,8 +229,8 @@ void _errorc(const uint8_t * xString)
 //-----------------------------------------------------------------------------
 void _info(const uint8_t * xString)
 {
-	_print(UC"\r\ni: ");
-	_print(xString);
+	_printString(UC"\r\ni: ");
+	_printString(xString);
 }
 
 //-----------------------------------------------------------------------------
@@ -202,5 +238,5 @@ void _info(const uint8_t * xString)
 //-----------------------------------------------------------------------------
 void _infoc(const uint8_t * xString)
 {
-	_print(xString);
+	_printString(xString);
 }
